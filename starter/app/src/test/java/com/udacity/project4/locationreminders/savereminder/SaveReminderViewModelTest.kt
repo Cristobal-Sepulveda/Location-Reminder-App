0e618eb92dcebc928924858780c3b354346ffbbb
Cristobal-Sepulveda <sepulveda.cristobal.ignacio@gmail.com>
2021-04-13 22:41:59 -0400

    test working in sdk 28

--- starter/app/src/test/java/com/udacity/project4/locationreminders/savereminder/SaveReminderViewModelTest.kt
+++ starter/app/src/test/java/com/udacity/project4/locationreminders/savereminder/SaveReminderViewModelTest.kt
@@ -1,16 +1,19 @@
 package com.udacity.project4.locationreminders.savereminder
 
 import androidx.test.ext.junit.runners.AndroidJUnit4
-
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import org.junit.Test
 import org.junit.runner.RunWith
+import org.junit.Assert.*
 
 @ExperimentalCoroutinesApi
 @RunWith(AndroidJUnit4::class)
 class SaveReminderViewModelTest {
 
-
     //TODO: provide testing to the SaveReminderView and its live data objects
-
+    @Test
+    fun saveReminderView_isWorking(){
+        assertEquals(4, 2+2)
+    }
 
 }
