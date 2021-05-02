bddabe4d54b2c3d538f8aa24de5ceb0fba2ce435
Cristobal-Sepulveda <sepulveda.cristobal.ignacio@gmail.com>
2021-05-02 12:46:17 -0400

    clickonView method updated

--- starter/app/src/androidTest/java/com/udacity/project4/locationreminders/savereminder/SaveReminderFragmentSnackAndToastTest.kt
+++ starter/app/src/androidTest/java/com/udacity/project4/locationreminders/savereminder/SaveReminderFragmentSnackAndToastTest.kt
@@ -1,10 +1,13 @@
 package com.udacity.project4.locationreminders.savereminder
 
+import android.app.Application
 import android.os.Bundle
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import androidx.fragment.app.testing.launchFragmentInContainer
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
+import androidx.test.core.app.ActivityScenario
+import androidx.test.core.app.ApplicationProvider
 import androidx.test.espresso.Espresso.closeSoftKeyboard
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.*
@@ -19,23 +22,34 @@ import androidx.test.rule.ActivityTestRule
 import com.udacity.project4.MainCoroutineRule
 import com.udacity.project4.R
 import com.udacity.project4.RemindersActivity
+import com.udacity.project4.locationreminders.data.ReminderDataSource
+import com.udacity.project4.locationreminders.data.local.LocalDB
+import com.udacity.project4.locationreminders.data.local.RemindersLocalRepository
+import com.udacity.project4.ui.reminderListFragment.RemindersListViewModel
 import com.udacity.project4.ui.saveReminderFragment.SaveReminderFragment
-import com.udacity.project4.util.DataBindingIdlingResource
-import com.udacity.project4.util.clickOnView
-import com.udacity.project4.util.monitorFragment
+import com.udacity.project4.ui.saveReminderFragment.SaveReminderViewModel
+import com.udacity.project4.util.*
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.runBlocking
 import kotlinx.coroutines.test.runBlockingTest
 import org.hamcrest.CoreMatchers.not
+import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
+import org.koin.androidx.viewmodel.dsl.viewModel
+import org.koin.core.context.startKoin
+import org.koin.core.context.stopKoin
+import org.koin.dsl.module
+import org.koin.test.AutoCloseKoinTest
+import org.koin.test.get
 import org.mockito.Mockito.mock
 
 @RunWith(AndroidJUnit4::class)
 @ExperimentalCoroutinesApi
 //UI Testing
 @MediumTest
-class SaveReminderFragmentSnackAndToastTest {
+class SaveReminderFragmentSnackAndToastTest: AutoCloseKoinTest() {
 
     @get:Rule
     val instantExecutorRule = InstantTaskExecutorRule()
@@ -48,9 +62,49 @@ class SaveReminderFragmentSnackAndToastTest {
         ActivityTestRule(RemindersActivity::class.java)
 
     private val dataBindingIdlingResource = DataBindingIdlingResource()
+    private lateinit var repository: ReminderDataSource
+    private lateinit var appContext: Application
+
+    /**
+     * As we use Koin as a Service Locator Library to develop our code, we'll also use Koin to test
+     * our code. at this step we will initialize Koin related code to be able to use it in out
+     * testing.
+     */
+    @Before
+    fun init() {
+        stopKoin()//stop the original app koin
+        appContext = ApplicationProvider.getApplicationContext()
+        val myModule = module {
+            viewModel {
+                RemindersListViewModel(
+                    appContext,
+                    get() as ReminderDataSource
+                )
+            }
+            single {
+                SaveReminderViewModel(
+                    appContext,
+                    get() as ReminderDataSource
+                )
+            }
+            single { RemindersLocalRepository(get()) as ReminderDataSource }
+            single { LocalDB.createRemindersDao(appContext) }
+        }
+        //declare a new koin module
+        startKoin {
+            modules(listOf(myModule))
+        }
+        //Get our real repository
+        repository = get()
+
+        //clear the data to start fresh
+        runBlocking {
+            repository.deleteAllReminders()
+        }
+    }
 
     @Test
-    fun snackbars_areWorking() {
+    fun snackbars_areWorking()=mainCoroutineRule.runBlockingTest {
         // Given
         val scenario = launchFragmentInContainer<SaveReminderFragment>(Bundle(), R.style.AppTheme)
         dataBindingIdlingResource.monitorFragment(scenario)
@@ -72,28 +126,24 @@ class SaveReminderFragmentSnackAndToastTest {
     @Test
     fun toast_isWorking()= mainCoroutineRule.runBlockingTest  {
         // Given
-        val scenario = launchFragmentInContainer<SaveReminderFragment>(Bundle(), R.style.AppTheme)
-        dataBindingIdlingResource.monitorFragment(scenario)
+        val scenario = ActivityScenario.launch(RemindersActivity::class.java)
+        dataBindingIdlingResource.monitorActivity(scenario)
         val navController = mock(NavController::class.java)
 
         // When
-        scenario.onFragment {
-            it.view?.let { view -> Navigation.setViewNavController(view, navController) }
-        }
+        onView(withId(R.id.addReminderFAB)).perform(click())
         onView(withId(R.id.reminderTitle)).perform(typeText("title"))
         closeSoftKeyboard()
         onView(withId(R.id.reminderDescription)).perform(typeText("description"))
         closeSoftKeyboard()
         onView(withId(R.id.selectLocation)).perform(click())
         onView(withId(R.id.map)).perform(longClick())
-
         /**
          * Method that clicks on a view, in the given coordinates, the coordinates start at
          * the top-left corner in 0,0
          */
-        onView(withId(R.id.map)).perform(clickOnView.clickIn(50,49))
+        onView(withId(R.id.map)).perform(clickPercent(51f,51f))
         onView(withId(R.id.savePOILatLgn_button)).perform(click())
-        Thread.sleep(3000)
         onView(withId(R.id.saveReminder))
 
         // Then
@@ -101,5 +151,6 @@ class SaveReminderFragmentSnackAndToastTest {
                 .inRoot(withDecorView(not(activityRule.activity
                         .window.decorView
                 ))).check(matches(isDisplayed()))
+    scenario.close()
     }
 }
--- starter/app/src/androidTest/java/com/udacity/project4/util/clickOnView.kt
+++ starter/app/src/androidTest/java/com/udacity/project4/util/clickOnView.kt
@@ -1,28 +1,24 @@
 package com.udacity.project4.util
 
-import android.view.InputDevice
-import android.view.MotionEvent
 import androidx.test.espresso.ViewAction
-import androidx.test.espresso.action.CoordinatesProvider
 import androidx.test.espresso.action.GeneralClickAction
 import androidx.test.espresso.action.Press
 import androidx.test.espresso.action.Tap
 
-object clickOnView {
-    fun clickIn(x: Int, y: Int): ViewAction {
-        return GeneralClickAction(
-            Tap.SINGLE,
-            CoordinatesProvider { view ->
-                val screenPos = IntArray(2)
-                view?.getLocationOnScreen(screenPos)
-
-                val screenX = (screenPos[0] + x).toFloat()
-                val screenY = (screenPos[1] + y).toFloat()
-
-                floatArrayOf(screenX, screenY)
-            },
-            Press.FINGER,
-            InputDevice.SOURCE_MOUSE,
-            MotionEvent.BUTTON_PRIMARY)
-    }
+fun clickPercent(pctX: Float, pctY: Float): ViewAction? {
+    return GeneralClickAction(
+        Tap.SINGLE,
+        { view ->
+            val screenPos = IntArray(2)
+            view.getLocationOnScreen(screenPos)
+            val w = view.width
+            val h = view.height
+            val x = w * pctX
+            val y = h * pctY
+            val screenX = screenPos[0] + x
+            val screenY = screenPos[1] + y
+            floatArrayOf(screenX, screenY)
+        },
+        Press.FINGER
+    )
 }
